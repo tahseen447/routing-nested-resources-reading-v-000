be743e5e40a1eae8f93d4d703cb55059b201c525
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2019-01-27 23:42:29 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def posts_index
+    @author = Author.find(params[:id])
+    @posts = @author.posts
+    render template: 'posts/index'
+  end
+
+  def post
+    @author = Author.find(params[:id])
+
+    # Note that because ids are unique by table we can go directly to
+    # Post.find — no need for @author.posts.find...
+    @post = Post.find(params[:post_id])
+    render template: 'posts/show'
+  end
 end
